b4059ad191be708d9f01db2c791aad6368bc59a8
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2017-09-15 16:25:20 +0300

    lecture 2: task

---
+++ src/main/scala/ru/spbau/jvm/scala/lecture02/Task.scala
@@ -0,0 +1,11 @@
+package ru.spbau.jvm.scala.lecture02
+
+object Task {
+
+  val text: String =
+    s"""
+       |Используя знания о разнообразном синтаксическом сахаре в Scala,
+       |  требуется наиболее Scala'образно переписать первое задание.
+     """.stripMargin
+
+}
